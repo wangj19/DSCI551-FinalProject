a01a377952af5d8c9c07b8cbd6c28ed050fbc463
unknown <wangj19@rose-hulman.edu>
2023-03-07 16:11:17 -0800

    add flask

--- mongos-server.py
+++ mongos-server.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
+from flask import Flask, jsonify, request, redirect, url_for
 
+# define app
+app = Flask(__name__)
 # connect to local MongoDB instance
 client = MongoClient('localhost', 27017)
 db = client['DSCI551']
@@ -11,3 +14,21 @@ collections = db.list_collection_names()
 print("Collections in database 'DSCI551':")
 for collection in collections:
     print(collection)
+
+@app.route('/',defaults={'myPath': ''})
+def homePage(myPath):
+    # display list of collections
+    toReturn = "Collections in database 'DSCI551':\n"
+    print("Collections in database 'DSCI551':")
+    for collection in collections:
+        print(collection)
+        toReturn = toReturn + str(collection) + "\n"
+    return toReturn
+    
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+# run app
+if __name__ == '__main__':
+   app.run(debug = True)
