9900682f8f9ff9fa5f957af999f460763c7d5662
unknown <wangj19@rose-hulman.edu>
2023-03-07 18:14:49 -0800

    add admin

--- mongos-server.py
+++ mongos-server.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
-from flask import Flask, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request, redirect, url_for, render_template, session
+from flask_socketio import SocketIO, emit
 
 # define app
 app = Flask(__name__)
+async_mode = None
+socket_ = SocketIO(app, async_mode=async_mode)
 # connect to local MongoDB instance
 client = MongoClient('localhost', 27017)
 db = client['DSCI551']
@@ -15,20 +18,24 @@ print("Collections in database 'DSCI551':")
 for collection in collections:
     print(collection)
 
-@app.route('/',defaults={'myPath': ''})
-def homePage(myPath):
+@app.route('/')
+def index():
     # display list of collections
     toReturn = "Collections in database 'DSCI551':\n"
-    print("Collections in database 'DSCI551':")
+    # print("Collections in database 'DSCI551':")
     for collection in collections:
-        print(collection)
-        toReturn = toReturn + str(collection) + "\n"
-    return toReturn
+        # print(collection)
+        toReturn = toReturn + str(collection) + "/\n"
+    return render_template("index.html", collection = toReturn, async_mode = socket_.async_mode)
     
 @app.route('/admin')
 def hello_admin():
-   return 'Hello Admin'
+   return render_template("admin.html", async_mode = socket_.async_mode)
+@socket_.on("command", namespace="/admin")
+def handle_command(command):
+    print(command)
+
 
 # run app
 if __name__ == '__main__':
-   app.run(debug = True)
+   socket_.run(app, debug=True)
