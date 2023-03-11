db44993a70ac3a0fd682d4daa56c00975373af44
unknown <wangj19@rose-hulman.edu>
2023-03-11 02:27:33 -0800

    admin page start uo

--- mongos-server.py
+++ mongos-server.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
-from flask import Flask, jsonify, request, redirect, url_for, render_template, session
+from flask import Flask, jsonify, request, render_template
 from flask_socketio import SocketIO, emit
 
 # define app
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder = "statics")
 async_mode = None
 socket_ = SocketIO(app, async_mode=async_mode)
 # connect to local MongoDB instance
@@ -13,10 +13,12 @@ db = client['DSCI551']
 # get list of collections in database
 collections = db.list_collection_names()
 
-# display list of collections
-print("Collections in database 'DSCI551':")
-for collection in collections:
-    print(collection)
+def command_process(command):
+    parsed_command = command.split(" ")
+    print(parsed_command)
+    if parsed_command[0].lower() != "curl":
+        return "Invalid Command: only accept curl command"
+    return parsed_command
 
 @app.route('/')
 def index():
@@ -27,14 +29,19 @@ def index():
         # print(collection)
         toReturn = toReturn + str(collection) + "/\n"
     return render_template("index.html", collection = toReturn, async_mode = socket_.async_mode)
-    
-@app.route('/admin')
-def hello_admin():
-   return render_template("admin.html", async_mode = socket_.async_mode)
-@socket_.on("command", namespace="/admin")
-def handle_command(command):
-    print(command)
+@app.route("/login")
+def login_page():
+    return render_template("login.html", async_mode = socket_.async_mode)
 
+@app.route('/admin', methods = ["GET","POST"])
+def admin_page():
+   return render_template("admin.html", async_mode = socket_.async_mode)
+
+@app.route("/handle_command", methods=["POST"])
+def handle_command():
+    command = request.form["command"]
+    results = command_process(command)
+    return(str(results))
 
 # run app
 if __name__ == '__main__':
