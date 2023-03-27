bfd46c6971a57c628d07d0bace7e1f836d1c5371
unknown <wangj19@rose-hulman.edu>
2023-03-27 13:38:59 -0700

    working on filter conditions

--- mongodb_funtions.py
+++ mongodb_funtions.py
@@ -3,18 +3,83 @@ import validators
 import json
 
 command_list = ["GET", "PUT", "POST", "PATCH", "DELETE"]
+filter_conds_list = ["orderBy", "limitToFirst", "limitToLast", "equalTo", "startAt", "endAt"]
 
 ## TODO: get document from db
 ## Currently can do get command without filter
 def process_GET(url, conditions):
     try: 
+        # curl -X GET "http://localhost:27017/DSCI551/books.json?orderBy='price'&limitToFirst=5"
+        # Process filter conditions for GET command
+        orderByIndex = None
+        orderByFlag = False
+        sortOrder = 0
+        limitToNumber = None
+        current_cond_key = []
+        find_query = dict()
+        if conditions is not None:
+            for cond in conditions.split("&"):
+                #check if the filter format is valid
+                if len(cond.split("="))<2:
+                    return "Invalid Command: inproper filter condition format"
+                filter_key = cond.split("=")[0]
+                filter_value = cond.split("=")[1]
+                # check if the filter condition is valid
+                if filter_key not in filter_conds_list:
+                    return "Invalid Command: invalid filter condition"
+                # check if the filter condition is duplicated
+                if filter_key in current_cond_key:
+                    return "Invalid Command: duplicated filter condition"
+                else:
+                    current_cond_key.append(filter_key)
+                
+                # adjust filter_value to corresponding type
+                if (filter_value[0]=="\'" and filter_value[-1]=="\'") or (filter_value[0]=="\"" and filter_value[-1]=="\""):
+                    filter_value = filter_value[1:-1]
+                elif filter_value.lower() == "true":
+                    filter_value = True
+                elif filter_value.lower() == "false":
+                    filter_value = False
+                elif "." in filter_value:
+                    filter_value = float(filter_value)
+                else:
+                    filter_value = int(filter_value)
+                # start to handle filters
+                if filter_key == "orderBy":
+                    orderByFlag = True
+                    orderByIndex = filter_value
+                elif filter_key == "limitToFirst":
+                    if orderByFlag is not True:
+                        return "Invalid Command: please enter orderBy condition first"
+                    elif sortOrder < 0 :
+                        return "Invalid Command: cannot enter limitToFirst and limitToLast together"
+                    elif type(filter_value):
+                        return "Invalid Command: limitToFirst only accepts integer"
+                    else:
+                        sortOrder = 1
+                        limitToNumber = filter_value
+                elif filter_key == "limitToFirst":
+                    if orderByFlag is not True:
+                        return "Invalid Command: please enter orderBy condition first"
+                    elif sortOrder > 0 :
+                        return "Invalid Command: cannot enter limitToFirst and limitToLast together"
+                    elif type(filter_value):
+                        return "Invalid Command: limitToLast only accepts integer"
+                    else:
+                        sortOrder = -1
+                        limitToNumber = filter_value
+                else:
+                    print("TODO")
+                    
+
+                print(filter_value)
+                print(type(filter_value))
+        
+        ## Get address and port to establish connection to mongodb
         parsed_url = url.split("/")
-        #print(str(parsed_url))
         address = "localhost"
         port = 27017
-        filter_conditions = dict()
-        if conditions is not None:
-            print("TODO: process filters")
+
         if len(parsed_url[0].split(":"))==2:
             address = parsed_url[0].split(":")[0]
             port = parsed_url[0].split(":")[1]
@@ -49,6 +114,7 @@ def process_GET(url, conditions):
                     db = client[db_name]
                     for col_name in db.list_collection_names():
                         col_content = []
+                        
                         for document in db[col_name].find({}):
                             col_content.append(document)
                             # print(document)
@@ -96,6 +162,8 @@ def process_GET(url, conditions):
         print("error occurs")
         return "Invalid Command: " + str(e)
 
+
+# High-level function for command processing
 def command_process(command):
     # parse command by spaces
     parsed_command = command.split(" ")
