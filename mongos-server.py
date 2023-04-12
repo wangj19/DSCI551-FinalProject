c215b2ccf540c3150c5da4d5043e9727b7946ebd
unknown <wangj19@rose-hulman.edu>
2023-04-12 13:58:35 -0700

    working on frontend

--- mongos-server.py
+++ mongos-server.py
@@ -12,16 +12,29 @@ db = client['DSCI551']
 
 # get list of collections in database
 collections = db.list_collection_names()
-
+books_collection = db["books"]
 @app.route('/')
 def index():
-    # display list of collections
-    toReturn = "Collections in database 'DSCI551':\n"
-    # print("Collections in database 'DSCI551':")
-    for collection in collections:
-        # print(collection)
-        toReturn = toReturn + str(collection) + "/\n"
-    return render_template("index.html", collection = toReturn, async_mode = socket_.async_mode)
+
+    books = books_collection.find({}, {"_id":0})
+    books_data = []
+    for book in books:
+        data = book[list(book.keys())[0]]
+        book_data = dict({"ISBN": list(book.keys())[0], "title": data["name"], "author": data["author"], 
+                          "price": data["price"], "description": data["description"]})
+        books_data.append(book_data)
+    return render_template('index.html', books=books_data)
+
+@app.route('/book/<isbn>')
+def book_detail(isbn):
+    document = books_collection.find({str(isbn): {"$exists": True}}, {"_id":0})
+    book_data = {}
+    for book in document:
+        data = book[list(book.keys())[0]]
+        book_data = dict({"ISBN": list(book.keys())[0], "title": data["name"], "author": data["author"], 
+                          "price": data["price"], "description": data["description"]})
+    return str(book_data)
+
 @app.route("/login")
 def login_page():
     return render_template("login.html", async_mode = socket_.async_mode)
